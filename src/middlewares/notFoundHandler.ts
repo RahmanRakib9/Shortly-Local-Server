e8fb7045aeb19c87a52445cae5ba62b0971a719c
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-04-09 23:00:22 +0600

    feat: implement http status package

--- src/middlewares/notFoundHandler.ts
+++ src/middlewares/notFoundHandler.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 
 export function notFoundHandler(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  res.status(404).json({
+  res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: 'Provided Credential is False!',
   });
