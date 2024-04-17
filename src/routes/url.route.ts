f9d2eefc3d796f5cbb481012442556572126cefb
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-04-17 13:43:26 +0600

    refactor: update the url route name

--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -3,11 +3,11 @@ import { urlControllers } from '../controllers/url.controller';
 
 const router = express.Router();
 
-router.post('/url', urlControllers.handleCreateNewShortURL);
+router.post('/urls', urlControllers.handleCreateNewShortURL);
 
 router.get('/urls', urlControllers.handleGetShortURLs);
 
-router.get('/:shortId', urlControllers.handleRedirectUserToGivenURL);
+router.get('/urls/:shortId', urlControllers.handleRedirectUserToGivenURL);
 
 router.get('/analytics/:shortId', urlControllers.handleGetAnalytics);
 
